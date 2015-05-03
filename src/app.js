c2a34569feba88a48b4a2fd7f020fdfa72e3062b
Robert Rees <shudderfix@gmail.com>
2015-05-03 23:32:21 +0100

    Adds the first controller

--- src/app.js
+++ src/app.js
@@ -3,5 +3,7 @@ console.log('Hello world');
 
 angular.module('app', [])
     .controller('MainCtrl', [function() {
+        var vm = this;
         console.log('Main Controller created');
+        this.message = "Hello from the controller";
     }]);
